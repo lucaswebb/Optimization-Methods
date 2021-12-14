66aadae4221387631f8db5666e752a1a97df6d9f
Lucas Webb <lucasawebb@gmail.com>
2021-12-14 12:31:55 -0700

    Updates

--- Driver.py
+++ Driver.py
@@ -4,7 +4,6 @@ from TrustRegion import *
 import matplotlib.pyplot as plt
 
 
-
 # Rosenbrock function
 a = 1
 b = 100
@@ -12,7 +11,8 @@ rosenbrock = lambda x : (a-x[0])**2 + b*(x[1]-x[0]**2)**2
 
 rosen_grad, rosen_hess = compute_gradient_hessian(rosenbrock, [x, y])
 
-trust_rosen_results = trust_region(rosenbrock, rosen_grad, rosen_hess, [5.0, 5.0])
+trust_rosen_results = trust_region(rosenbrock, rosen_grad, rosen_hess, [5.0, 5.0], [1, 1])
+print('Trust Region # of iterations:', trust_rosen_results[1])
 
 
 ##Test Functions
@@ -26,6 +26,7 @@ def rosen_jacobian(x):
 
 ##Executions
 
+plt.figure(1)
 sol=np.matrix([1.,1.]).T
 # Quadratic
 [x,n,log,er]=linetrace(rosenbrock2d,rosen_jacobian,quadratic,gradg,[5.0,5.0],15., sol)
@@ -36,14 +37,47 @@ plt.plot(log[0],log[1],'b', label='Quadratic')
 plt.plot(log[0],log[1],'m', label='Wolfe')
 
 #Trust Region
-plt.plot(trust_rosen_results[2], label='Trust-Region')
+plt.plot([item[0] for item in trust_rosen_results[2]], label='Trust-Region')
 
 
-# plt.xscale("log")
-plt.yscale("log")
 plt.legend()
 plt.title("Rosenbrock starting at (5,5)")
-# plt.xlim((0, 7500))
-plt.xlabel('# of Iterations')
+plt.yscale("log")
 plt.ylabel('Log of Absolute Error')
+# plt.xscale("log")
+# plt.xlabel('Log of # of Iterations')
+plt.xlabel('# of Iterations')
+# plt.xlim((0, 7500))
 plt.show()
+
+plt.figure(2)
+plt.plot([item[0] for item in trust_rosen_results[2]], [item[1] for item in trust_rosen_results[2]], label='Trust-Region')
+plt.xscale('log')
+plt.gca().invert_xaxis()
+plt.title('Rosenbrock starting at (5,5)')
+plt.xlabel('Log of absolute error')
+plt.ylabel('# of Function Evaluations')
+plt.legend()
+plt.show()
+
+plt.figure(3)
+plt.plot([item[0] for item in trust_rosen_results[2]], [item[2] for item in trust_rosen_results[2]], label='Trust-Region')
+plt.xscale('log')
+plt.gca().invert_xaxis()
+plt.title('Rosenbrock starting at (5,5)')
+plt.xlabel('Log of absolute error')
+plt.ylabel('# of Gradient Evaluations')
+plt.legend()
+plt.show()
+
+plt.figure(4)
+plt.plot([item[0] for item in trust_rosen_results[2]], [item[3] for item in trust_rosen_results[2]], label='Trust-Region')
+plt.xscale('log')
+plt.gca().invert_xaxis()
+plt.title('Rosenbrock starting at (5,5)')
+plt.xlabel('Log of absolute error')
+plt.ylabel('# of Hessian Evaluations')
+plt.legend()
+plt.show()
+
+print(trust_rosen_results)
--- TrustRegion.py
+++ TrustRegion.py
@@ -27,7 +27,7 @@ def find_tau(z_j, d_j, radius):
 
 # page 171 in Nocedal
 # find Tau directly by using the quadratic formula
-def steihaug(radius, grad_k, hess_k, eps=1e-8):
+def steihaug(radius, grad_k, hess_k, eps=1e-15):
     z_j = np.zeros(grad_k.size)
     r_j = np.copy(grad_k)
     d_j = -np.copy(grad_k)
@@ -66,7 +66,7 @@ def steihaug(radius, grad_k, hess_k, eps=1e-8):
 
 # n must be between 0 and 0.25
 # from Nocedal page 69
-def trust_region(f, f_grad, f_hess, x_0, tol=1e-15, model=quadratic_model, radius_0=1.0, radius_max=300.0, n=1e-3):
+def trust_region(f, f_grad, f_hess, x_0, sol, tol=1e-15, model=quadratic_model, radius_0=1.0, radius_max=300.0, n=1e-3):
     k = 0
     x_k = x_0
     radius = radius_0
@@ -77,15 +77,20 @@ def trust_region(f, f_grad, f_hess, x_0, tol=1e-15, model=quadratic_model, radiu
     hess_k = f_hess(x_k)
     model_k = model(f_k, grad_k, hess_k)
 
-    error = []
-    error.append(np.linalg.norm(np.subtract(x_0, [1,1])))
+    num_f_eval = 1
+    num_grad_eval = 1
+    num_hess_eval = 1
 
-    while np.linalg.norm(f_grad(x_k)) > tol:
+    error = []
+    error.append([np.linalg.norm(np.subtract(x_0, sol)), num_f_eval, num_grad_eval, num_hess_eval])
+
+    while np.linalg.norm(grad_k) > tol:
         # solve subproblem using Steihaug's Method
         p_k = steihaug(radius, grad_k, hess_k)
 
         # evaluate agreement between model function and actual function
-        rho_k = (f(x_k) - f(x_k + p_k)) / (f(x_k) - model_k(p_k))
+        rho_k = (f(x_k) - f(x_k + p_k)) / (model_k(np.zeros(grad_k.size)) - model_k(p_k))
+        num_f_eval += 2
 
         if rho_k < 0.25:
             # poor approximation so shrink the trust-radius
@@ -105,10 +110,15 @@ def trust_region(f, f_grad, f_hess, x_0, tol=1e-15, model=quadratic_model, radiu
             hess_k = f_hess(x_k)
             model_k = model(f_k, grad_k, hess_k)
 
-        k += 1
-        error.append(np.linalg.norm(np.subtract(x_k, [1, 1])))
+            num_f_eval += 1
+            num_grad_eval += 1
+            num_hess_eval += 1
 
-        if np.linalg.norm(p_k) < tol:
+        k += 1
+        error.append([np.linalg.norm(np.subtract(x_k, sol)), num_f_eval, num_grad_eval, num_hess_eval])
+
+        if np.linalg.norm(np.subtract(x_k, sol)) < tol:
             break
 
+    # final value, # of iterations, [error, num f evals, num grad evals, num hess evals] by iteration
     return [x_k, k, error]
